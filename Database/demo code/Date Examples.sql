2bb5159256b06e48bdd8aa99b86e815d7067c4e4
CodyHenrichsen-CTEC <cody.henrichsen@canyonsdistrict.org>
2022-02-16 12:31:07 -0700

    Added date based demo questions.
    Using date comparison in the WHERE clause to limit results, extracting information from dates and intervals, using the OVERLAPS command, demonstrating what DOES NOT work!

---
+++ Database/demo code/Date Examples.sql
@@ -0,0 +1,134 @@
+-- Using dates in a WHERE clause
+
+-- Before: 10 rows
+SELECT
+   prodname AS product_name,
+   prodnextshipdate AS next_ship_date
+FROM
+    product;
+
+-- After 
+-- using WHERE: To find days or years with 07
+SELECT
+    prodname AS product_name,
+    prodnextshipdate AS next_ship_date
+FROM
+    product
+WHERE
+    TO_CHAR(prodnextshipdate, 'DD-YY') LIKE '%07%';
+
+-- using WHERE: To find February as the month
+SELECT
+    prodname AS product_name,
+    prodnextshipdate AS next_ship_date
+FROM
+    product
+WHERE
+    TO_CHAR(prodnextshipdate, 'MON') = 'FEB';
+
+-- Evaluate a date as text
+SELECT
+    prodname AS product_name,
+    prodnextshipdate AS next_ship_date
+FROM
+    product
+WHERE
+    INSTR(prodnextshipdate ,'JAN') > 0;
+
+-- But if looking at the masked data nopers
+SELECT
+    prodname AS product_name,
+    prodnextshipdate AS next_ship_date
+FROM
+    product
+WHERE
+    INSTR(TO_CHAR(prodnextshipdate, 'DD/MM/YYYY') ,'JAN') > 0;
+
+-- Nope Section! 
+
+-- Can't just dump crap into the TO_CHAR function
+SELECT
+    TO_CHAR(SYSDATE, 'MM/DD/YYYY Day' || ' the ' || 'DDspth' || ' of ' || 'Month, YYYY') AS bizarre_request
+FROM
+    dual;
+
+-- Note that the ridiculous query length can be split over multiple lines to make it much easier to read
+SELECT
+    TO_CHAR(SYSDATE, 'MM/DD/YYYY Day' ) || ' the '  
+    || TO_CHAR(SYSDATE, 'DDspth') || ' of ' 
+    || TO_CHAR(SYSDATE, 'Month, YYYY') AS overdone_but_valid
+FROM
+    dual;
+
+-- Using the INTERVAL data types in Oracle
+
+-- Intervals interact with dates
+
+-- Too much effort to add time!
+SELECT
+    EXTRACT (MONTH FROM TO_YMINTERVAL('3-05')) AS month_info,
+    ADD_MONTHS(SYSDATE, EXTRACT (MONTH FROM TO_YMINTERVAL('3-05'))) AS date_interval_manip
+FROM
+    dual;
+
+--     
+SELECT
+    'Cody' AS name,
+    TO_CHAR(TO_DATE('06-19-1995','MM-DD-YYYY'), 'fmMonth fmDdspth, Year') AS special_day,    
+    TO_CHAR(SYSDATE - TO_YMINTERVAL('5-11'), 'DD MM YYYY') || ' was almost six years ago!' AS interval_demo,
+    TO_CHAR(TO_DATE('09-30-4321','MM-DD-YYYY'), 'Day Month Year') || ' is not right now lol' AS demo_two
+FROM
+    dual;
+
+SELECT
+    ADD_MONTHS(SYSDATE, -4) AS backward,
+    TO_CHAR(SYSDATE - TO_YMINTERVAL('0-4'), 'DD MM YYYY') || ' was four months ago!' AS interval_demo,
+    TO_CHAR(SYSDATE + TO_DSINTERVAL('3 4:45:32'), 'DD MM YYYY') || ' is three days and a few hours from now!' AS interval_demo_two
+FROM
+    dual;
+-- note the requirement of contiguous time
+--error version
+SELECT
+    EXTRACT (DAY FROM INTERVAL '3:2' DAY TO SECOND) AS error_version
+FROM
+    dual;
+
+--corect version
+SELECT
+    EXTRACT (DAY FROM INTERVAL '03 02:45:21' DAY TO SECOND) AS corrected_version
+FROM
+    dual;
+
+-- Can't use EXTRACT on a DATE!
+SELECT
+    prodname AS product_name,
+    prodnextshipdate AS next_ship_date
+FROM
+    product;
+WHERE
+    EXTRACT MONTH FROM prodnextshipdate = 2;
+-- Still NOPERS
+SELECT
+    prodname AS product_name,
+    prodnextshipdate AS next_ship_date,
+    EXTRACT MONTH FROM prodnextshipdate AS month_data
+FROM
+    product;
+
+-- Using the OVERLAPS comparison
+SELECT
+   ordname AS customer_name,
+   TO_CHAR(orddate, 'DD Day') AS date_day 
+FROM
+    ordertbl;
+
+SELECT
+   ordname AS customer_name,
+   TO_CHAR(orddate, 'DD Day') AS date_day 
+FROM
+    ordertbl
+WHERE
+    (orddate, orddate) 
+    OVERLAPS 
+    (TO_DATE('10/01/2007','DD/MM/YYYY'), TO_YMINTERVAL('0-1'))
+;
