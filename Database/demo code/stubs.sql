8f5b71f8cec632f0890cecccf7c0f69e8a9f1171
Cody Henrichsen <cody.henrichsen@canyonsdistrict.org>
2025-02-19 12:12:30 -0700

    Added stubs demo file

---
+++ Database/demo code/stubs.sql
@@ -0,0 +1,14 @@
+-- NAME
+-- DATE
+-- Assignment name
+
+-- VALID STUB
+-- 1 -- Must include the problem number!
+-- May include other notes embedded in comments to help you with the query
+SELECT
+    firstName -- Must include a valid field for the query
+FROM
+    students -- Must include a reasonable and valid table for field and query.
+; -- Must end with a semicolon!
+
+-- Repeat for ALL problems in the assignment!
